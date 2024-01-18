dbdd3ef3157b1c5c90c82b5bb6547c0b3fade325
sahand <s.jalilvand@omid.ir>
2024-01-18 10:38:06 +0330

    init log

--- src/main/java/com/example/subscription/SubscriptionApplication.java
+++ src/main/java/com/example/subscription/SubscriptionApplication.java
@@ -1,13 +1,22 @@
 package com.example.subscription;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
+@Slf4j
 public class SubscriptionApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SubscriptionApplication.class, args);
     }
 
+    @Bean
+    public CommandLineRunner init(@Value("${spring.application.name}") String appName) {
+        return args -> log.info("{} is running!", appName);
+    }
 }
