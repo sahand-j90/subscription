aa123e6ce383c3df001cd9269711f556b4d71e04
sahand <s.jalilvand@omid.ir>
2024-01-20 21:23:20 +0330

    DML for admin user

--- src/main/resources/migration/dml/add_admin_user.sql
+++ src/main/resources/migration/dml/add_admin_user.sql
@@ -5,6 +5,6 @@
 INSERT INTO tlb_user (username, created_at, updated_at, optimistic_lock_version, is_enabled, password)
     VALUES ('Admin', '2024-01-20 21:02:30.000000', null, 0, true, '$2a$10$D5dF8p2kyF8xdLA.nUXpYuZyjH8a03b/bV7nBhDAWiwFH9tGdcln6');
 
-INSERT INTO tbl_user_authority (username, authority) VALUES ('Admin', 'Admin');
+INSERT INTO tbl_user_authority (username, authority) VALUES ('Admin', 'ADMIN');
 
 -- Last line of change set
